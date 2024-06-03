e9751612732c3d671bb9ba77c898f185155d34b7
Your Name <you@example.com>
2024-06-03 11:15:34 +0200

    frist commit

--- settings.py
+++ settings.py
@@ -1,10 +1,9 @@
 # This variable will be True only when running the code from our local machine
-is_local_environment = False # Set to False before deploying to Cloud Functions
+is_local_environment = True # Set to False before deploying to Cloud Functions
+cloud_storage_service_account = "" # Replace with your Cloud Storage service account filename
+bigquery_service_account = "" # Replace with your BigQuery service account filename
 
-#cloud_storage_service_account = r"C:\Users\pasat\OneDrive\Υπολογιστής\machine_learning\Weather_app\weatheranalysisproject-288b95e658cc.json"
-#bigquery_service_account = r"C:\Users\pasat\OneDrive\Υπολογιστής\machine_learning\Weather_app\weatheranalysisproject-288b95e658cc.json"
-
-api_key = 'bbcfd621f8689a0d174c292e0889afab'
-project_id = 'weatheranalysisproject'
-dataset_id= 'weather_api'
-bucket_name = 'bucket-for-weather'
+api_key = '' # Replace with your OpenWeather API key
+project_id = '' # Replace with your GCP Project ID
+dataset_id= '' # Replace with your BigQuery Dataset Name
+bucket_name = '' # Replace with your globally unique bucket name
